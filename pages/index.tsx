eb728ae3d603493055ddbe9b7c4430e5dd32fe18
Danae Harris <hello@danaeharris.com>
2023-02-02 21:46:49 -0500

    Begin adding placeholder content.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,123 +1,268 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
+import logo from "../public/logo.png";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [books, setBooks] = useState([
+    {
+      id: 1342,
+      title: "Pride and Prejudice",
+      authors: [
+        {
+          name: "Austen, Jane",
+          birth_year: 1775,
+          death_year: 1817,
+        },
+      ],
+      translators: [],
+      subjects: [
+        "Courtship -- Fiction",
+        "Domestic fiction",
+        "England -- Fiction",
+        "Love stories",
+        "Sisters -- Fiction",
+        "Social classes -- Fiction",
+        "Young women -- Fiction",
+      ],
+      bookshelves: ["Best Books Ever Listings", "Harvard Classics"],
+      languages: ["en"],
+      copyright: false,
+      media_type: "Text",
+      formats: {
+        "application/octet-stream":
+          "https://www.gutenberg.org/files/1342/1342-0.zip",
+        "text/plain; charset=us-ascii":
+          "https://www.gutenberg.org/files/1342/1342-0.txt",
+        "text/plain": "https://www.gutenberg.org/ebooks/1342.txt.utf-8",
+        "application/x-mobipocket-ebook":
+          "https://www.gutenberg.org/ebooks/1342.kf8.images",
+        "application/epub+zip":
+          "https://www.gutenberg.org/ebooks/1342.epub3.images",
+        "image/jpeg":
+          "https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg",
+        "text/html": "https://www.gutenberg.org/ebooks/1342.html.images",
+        "application/rdf+xml": "https://www.gutenberg.org/ebooks/1342.rdf",
+      },
+      download_count: 57315,
+    },
+    {
+      id: 64317,
+      title: "The Great Gatsby",
+      authors: [
+        {
+          name: "Fitzgerald, F. Scott (Francis Scott)",
+          birth_year: 1896,
+          death_year: 1940,
+        },
+      ],
+      translators: [],
+      subjects: [
+        "First loves -- Fiction",
+        "Long Island (N.Y.) -- Fiction",
+        "Married women -- Fiction",
+        "Psychological fiction",
+        "Rich people -- Fiction",
+      ],
+      bookshelves: [],
+      languages: ["en"],
+      copyright: false,
+      media_type: "Text",
+      formats: {
+        "application/x-mobipocket-ebook":
+          "https://www.gutenberg.org/ebooks/64317.kf8.images",
+        "application/epub+zip":
+          "https://www.gutenberg.org/ebooks/64317.epub3.images",
+        "text/html": "https://www.gutenberg.org/ebooks/64317.html.images",
+        "image/jpeg":
+          "https://www.gutenberg.org/cache/epub/64317/pg64317.cover.medium.jpg",
+        "application/octet-stream":
+          "https://www.gutenberg.org/files/64317/64317-0.zip",
+        "text/plain": "https://www.gutenberg.org/ebooks/64317.txt.utf-8",
+        "text/plain; charset=us-ascii":
+          "https://www.gutenberg.org/files/64317/64317-0.txt",
+        "application/rdf+xml": "https://www.gutenberg.org/ebooks/64317.rdf",
+      },
+      download_count: 20487,
+    },
+
+    {
+      id: 45,
+      title: "Anne of Green Gables",
+      authors: [
+        {
+          name: "Montgomery, L. M. (Lucy Maud)",
+          birth_year: 1874,
+          death_year: 1942,
+        },
+      ],
+      translators: [],
+      subjects: [
+        "Bildungsromans",
+        "Canada -- History -- 1867-1914 -- Fiction",
+        "Country life -- Prince Edward Island -- Fiction",
+        "Friendship -- Fiction",
+        "Girls -- Fiction",
+        "Islands -- Fiction",
+        "Orphans -- Fiction",
+        "Prince Edward Island -- History -- 20th century -- Fiction",
+        "Shirley, Anne (Fictitious character) -- Fiction",
+      ],
+      bookshelves: ["Canada", "Children's Literature"],
+      languages: ["en"],
+      copyright: false,
+      media_type: "Text",
+      formats: {
+        "application/x-mobipocket-ebook":
+          "https://www.gutenberg.org/ebooks/45.kf8.images",
+        "application/epub+zip":
+          "https://www.gutenberg.org/ebooks/45.epub3.images",
+        "text/html": "https://www.gutenberg.org/ebooks/45.html.images",
+        "image/jpeg":
+          "https://www.gutenberg.org/cache/epub/45/pg45.cover.medium.jpg",
+        "application/octet-stream":
+          "https://www.gutenberg.org/files/45/45-0.zip",
+        "text/plain": "https://www.gutenberg.org/ebooks/45.txt.utf-8",
+        "text/plain; charset=us-ascii":
+          "https://www.gutenberg.org/files/45/45-0.txt",
+        "application/rdf+xml": "https://www.gutenberg.org/ebooks/45.rdf",
+      },
+      download_count: 9192,
+    },
+    {
+      id: 2488,
+      title:
+        "Twenty Thousand Leagues Under the Seas: An Underwater Tour of the World",
+      authors: [
+        {
+          name: "Verne, Jules",
+          birth_year: 1828,
+          death_year: 1905,
+        },
+      ],
+      translators: [
+        {
+          name: "Walter, Frederick Paul",
+          birth_year: 1943,
+          death_year: null,
+        },
+      ],
+      subjects: [
+        "Adventure stories",
+        "Science fiction",
+        "Sea stories",
+        "Submarines (Ships) -- Fiction",
+        "Underwater exploration -- Fiction",
+      ],
+      bookshelves: ["Movie Books", "Science Fiction"],
+      languages: ["en"],
+      copyright: true,
+      media_type: "Text",
+      formats: {
+        "application/x-mobipocket-ebook":
+          "https://www.gutenberg.org/ebooks/2488.kf8.images",
+        "application/epub+zip":
+          "https://www.gutenberg.org/ebooks/2488.epub3.images",
+        "text/html; charset=utf-8":
+          "https://www.gutenberg.org/files/2488/2488-h/2488-h.htm",
+        "image/jpeg":
+          "https://www.gutenberg.org/cache/epub/2488/pg2488.cover.medium.jpg",
+        "text/html": "https://www.gutenberg.org/ebooks/2488.html.images",
+        "text/plain; charset=utf-8":
+          "https://www.gutenberg.org/files/2488/2488-0.txt",
+        "application/rdf+xml": "https://www.gutenberg.org/ebooks/2488.rdf",
+      },
+      download_count: 844,
+    },
+    {
+      id: 55,
+      title: "The Wonderful Wizard of Oz",
+      authors: [
+        {
+          name: "Baum, L. Frank (Lyman Frank)",
+          birth_year: 1856,
+          death_year: 1919,
+        },
+      ],
+      translators: [],
+      subjects: [
+        "Courage -- Juvenile fiction",
+        "Cowardly Lion (Fictitious character) -- Juvenile fiction",
+        "Cyclones -- Juvenile fiction",
+        "Dreams -- Juvenile fiction",
+        "Fantasy literature",
+        "Gale, Dorothy (Fictitious character) -- Juvenile fiction",
+        "Good and evil -- Juvenile fiction",
+        "Home -- Juvenile fiction",
+        "Oz (Imaginary place) -- Juvenile fiction",
+        "Scarecrow (Fictitious character from Baum) -- Juvenile fiction",
+        "Tin Woodman (Fictitious character) -- Juvenile fiction",
+        "Toto (Fictitious character) -- Juvenile fiction",
+        "Witches -- Juvenile fiction",
+        "Wizard of Oz (Fictitious character) -- Juvenile fiction",
+      ],
+      bookshelves: ["Children's Literature", "Fantasy"],
+      languages: ["en"],
+      copyright: false,
+      media_type: "Text",
+      formats: {
+        "application/x-mobipocket-ebook":
+          "https://www.gutenberg.org/ebooks/55.kf8.images",
+        "application/epub+zip":
+          "https://www.gutenberg.org/ebooks/55.epub3.images",
+        "image/jpeg":
+          "https://www.gutenberg.org/cache/epub/55/pg55.cover.medium.jpg",
+        "text/html; charset=utf-8":
+          "https://www.gutenberg.org/files/55/55-h/55-h.htm",
+        "text/html": "https://www.gutenberg.org/ebooks/55.html.images",
+        "text/plain; charset=utf-8":
+          "https://www.gutenberg.org/files/55/55-0.txt",
+        "application/rdf+xml": "https://www.gutenberg.org/ebooks/55.rdf",
+      },
+      download_count: 7558,
+    },
+  ]);
+
+  console.log({ books });
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>GutenReads</title>
+        <meta
+          name="description"
+          content="A clean, beautiful interface for reading public-domain Project Gutenberg books."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
+      <main className="h-screen bg-white">
+        <div className="container p-4 md:p-6 lg:py-20">
           <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+            src={logo}
+            alt="GutenReads logo"
+            className="w-full sm:w-[297px]"
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <p className="leading-normal">
+            A clean, beautiful interface for reading public-domain Project
+            Gutenberg books.
+          </p>
+          <div className="py-4">
+            <h2>A few recommendations . . .</h2>
+            <div>
+              {books.map((book) => (
+                <div key={book.id}>
+                  <h3>{book.title}</h3>
+                  <p>{book.authors[0].name}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
-  )
+  );
 }
