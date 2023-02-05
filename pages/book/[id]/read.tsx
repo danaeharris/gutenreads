171c08697efb06f72b6a30f08f792fed1a624b2d
Danae Harris <hello@danaeharris.com>
2023-02-05 12:37:14 -0500

    Add header on reader page.

--- pages/book/[id]/read.tsx
+++ pages/book/[id]/read.tsx
@@ -70,10 +70,99 @@ export default function ReaderView() {
           background: `linear-gradient(180deg, ${book.gradientColor} 0%, rgba(255,255,255,1) 350px)`,
         }}
       >
-        <div className="fixed top-0 left-0 right-0 bg-white p-4">
-          Progress {percentageDone}% {hoursLeft} hours left
+        <div
+          className="fixed top-0 left-0 right-0 bg-white flex flex-row items-stretch justify-between"
+          style={{ boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)" }}
+        >
+          <button onClick={() => router.back()} className="p-4 sm:px-6">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Back Button</title>
+              <path
+                d="M15 18L9 12L15 6"
+                stroke="black"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+          <div className="border-x border-gray-150 py-2 px-6 hidden sm:flex sm:flex-row ">
+            {book.formats && book.formats["image/jpeg"] ? (
+              <Image
+                src={book.formats["image/jpeg"]}
+                alt={`${book.title} cover art`}
+                width={100}
+                height={150}
+                className="fill object-contain w-auto h-[52px] min-w-[36px] mr-3"
+                style={{
+                  filter: "drop-shadow(4px 8px 15px rgba(0, 0, 0, 0.25))",
+                }}
+              />
+            ) : null}
+            <div className="flex flex-col items-start justify-center">
+              <h5 className="text-md font-normal font-inter">{book.title}</h5>
+              <p className="text-sm font-inter">
+                by{" "}
+                {book.authors.map((author: Author) => (
+                  <span key={author.name}>
+                    {`${author.name.split(",")[1].split("(")[0]} ${
+                      author.name.split(",")[0].split("(")[0]
+                    }`}
+                    {book.authors.length > 1 ? ", " : ""}
+                  </span>
+                ))}
+              </p>
+            </div>
+          </div>
+          <div className="py-2 px-4 sm:px-6 h-full border-x border-gray-150 sm:border-none flex flex-row items-center justify-start self-center flex-1">
+            <svg
+              className="mr-4 stroke-current"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Open Book Icon</title>
+              <path
+                d="M2 3H8C9.06087 3 10.0783 3.42143 10.8284 4.17157C11.5786 4.92172 12 5.93913 12 7V21C12 20.2044 11.6839 19.4413 11.1213 18.8787C10.5587 18.3161 9.79565 18 9 18H2V3Z"
+                stroke=""
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M22 3H16C14.9391 3 13.9217 3.42143 13.1716 4.17157C12.4214 4.92172 12 5.93913 12 7V21C12 20.2044 12.3161 19.4413 12.8787 18.8787C13.4413 18.3161 14.2044 18 15 18H22V3Z"
+                stroke=""
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <div className="flex flex-col w-full">
+              <p className="mb-1">
+                Progress {percentageDone}% {hoursLeft} hours left
+              </p>
+              <div className="w-full h-1.5 bg-gray-150 rounded-full">
+                <div
+                  className="h-full bg-gray-900 rounded-full"
+                  style={{ width: `${percentageDone}%` }}
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col align-center justify-center py-2 px-4 sm:px-6 border-transparent sm:border-l sm:border-gray-150">
+            <p className="font-ppwriter text-2xl">Aa</p>
+          </div>
         </div>
-        <div className="container py-4 md:py-6 lg:py-20 flex flex-col items-center justify-start">
+        {/* page */}
+        <div className="container py-24 md:py-32 lg:py-32 flex flex-col items-center justify-start">
           <Image
             src={`/books/${book.title
               .replaceAll(" ", "-")
@@ -87,7 +176,7 @@ export default function ReaderView() {
             <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-6xl font-bold text-center pb-5">
               {book.title}
             </h1>
-            <p className="text-md font-ppwriter pb-16">
+            <p className="text-md font-ppwriter pb-10 md:pb-16">
               by{" "}
               {book.authors.map((author: Author) => (
                 <span key={author.name}>
