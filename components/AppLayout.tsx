4bb32883c60f43cddef4c240dba34fa85c29872a
Danae Harris <hello@danaeharris.com>
2023-02-05 16:02:40 -0500

    Add meta tags.

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -37,6 +37,15 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
           content="A clean, beautiful interface for reading public-domain Project Gutenberg books."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="GutenReads" />
+        <meta property="og:type" content="ebooks" />
+        <meta
+          property="og:description"
+          content="A clean, beautiful interface for reading public-domain Project Gutenberg books."
+        />
+        <meta property="og:image" content="./meta.jpg" />
+        <meta property="og:url" content="https://gutenreads.com/" />
+        <meta name="twitter:card" content="summary_large_image" />
         {/* import favicon.jpg as favicon */}
         <link rel="icon" href="/favicon.jpg" />
       </Head>
