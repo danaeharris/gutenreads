ff6c0bc31a77dcc0d652e2d4d97fa615a7fbd480
Danae Harris <hello@danaeharris.com>
2023-02-04 21:10:43 -0500

    Update modal.

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,10 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-
+Modal.defaultStyles.overlay.backgroundColor = "rgba(0, 0, 0, 0.25)";
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement("#app");
+
 function BookModal({
   isOpen,
   closeModal,
@@ -84,51 +85,51 @@ function BookModal({
             ) : null}
           </div>
         </div>
-      </div>
-      <div className="px-4 py-3 sm:flex sm:flex-row sm:px-6">
-        <Link
-          href={`/book/${book.id}/read`}
-          onClick={() => {
-            closeModal();
-          }}
-          type="button"
-          className="flex w-full justify-center items-center rounded-md border-2 border-transparent bg-black px-4 py-2 font-3xl font-medium text-white shadow-sm hover:bg-white  hover:border-black hover:text-black focus:outline-none focus:ring-2 focus:ring-black-500 sm:w-auto sm:text-sm"
-        >
-          <svg
-            className="mr-2 stroke-current"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="px-4 py-3 sm:flex sm:flex-row sm:px-6">
+          <Link
+            href={`/book/${book.id}/read`}
+            onClick={() => {
+              closeModal();
+            }}
+            type="button"
+            className="flex w-full justify-center items-center rounded-md border-2 border-transparent bg-black px-4 py-2 font-3xl font-medium text-white shadow-sm hover:bg-white  hover:border-black hover:text-black focus:outline-none focus:ring-2 focus:ring-black-500 sm:w-auto sm:text-sm"
           >
-            <path
-              d="M2 3H8C9.06087 3 10.0783 3.42143 10.8284 4.17157C11.5786 4.92172 12 5.93913 12 7V21C12 20.2044 11.6839 19.4413 11.1213 18.8787C10.5587 18.3161 9.79565 18 9 18H2V3Z"
-              stroke=""
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M22 3H16C14.9391 3 13.9217 3.42143 13.1716 4.17157C12.4214 4.92172 12 5.93913 12 7V21C12 20.2044 12.3161 19.4413 12.8787 18.8787C13.4413 18.3161 14.2044 18 15 18H22V3Z"
-              stroke=""
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <span className="m-0">Start Reading</span>
-        </Link>
+            <svg
+              className="mr-2 stroke-current"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2 3H8C9.06087 3 10.0783 3.42143 10.8284 4.17157C11.5786 4.92172 12 5.93913 12 7V21C12 20.2044 11.6839 19.4413 11.1213 18.8787C10.5587 18.3161 9.79565 18 9 18H2V3Z"
+                stroke=""
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M22 3H16C14.9391 3 13.9217 3.42143 13.1716 4.17157C12.4214 4.92172 12 5.93913 12 7V21C12 20.2044 12.3161 19.4413 12.8787 18.8787C13.4413 18.3161 14.2044 18 15 18H22V3Z"
+                stroke=""
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <span className="m-0">Start Reading</span>
+          </Link>
 
-        <button
-          onClick={() => {
-            closeModal();
-          }}
-          type="button"
-          className="m-3 inline-flex"
-        >
-          Cancel
-        </button>
+          <button
+            onClick={() => {
+              closeModal();
+            }}
+            type="button"
+            className="m-3 inline-flex"
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     </Modal>
   );
