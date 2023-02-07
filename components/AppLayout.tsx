307adf855e84770a54e35245cb8ca56d7452577a
Danae Harris <hello@danaeharris.com>
2023-02-06 20:25:28 -0500

    Clean up unused code.

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,31 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import { useEffect, useState } from "react";
-import logo from "../public/logo.png";
 import Footer from "@/components/AppFooter";
-
-export type Book = {
-  id: number;
-  title: string;
-  authors: Author[];
-  translators: any;
-  subjects: string[];
-  bookshelves: string[];
-  languages: string[];
-  copyright: boolean;
-  media_type: string;
-  formats: {
-    [key: string]: string;
-  };
-  download_count: number;
-};
-
-export type Author = {
-  name: string;
-  birth_year: number;
-  death_year: number;
-};
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
